fd2db91071aeb8d405df00561b7fd6812d76fb54
KuKu <khukhu113@gmail.com>
2022-02-19 16:58:16 +0630

    Commit

---
+++ StateDataManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StateDataManagement
+//
+//  Created by Ku Ku on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
